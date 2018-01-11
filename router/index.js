d96308ecda78a45f3932491b763b8125cec73b68
zhoujin <1391020381@qq.com>
2018-01-11 09:30:55 +0800

    change

--- router/index.js
+++ router/index.js
@@ -8,5 +8,6 @@ router.use('/', home.routes(), home.allowedMethods())
 router.use('/page', page.routes(), page.allowedMethods())
 router.use('/post/form', post.routes(), page.allowedMethods())
 
+
 // home(home.js),page(page.js) 都是单个自路由, router(index.js)是路由的入口。 router.use('/page',page.routes,page.allowedMethods())  /page是为所有page.js中的路由加上/page前缀
 module.exports = router
