512286a4d020d91c085b6a299abb1abb56d5e879
行云流水justdoit <justdoit>
2018-01-14 16:20:42 +0800

    static

--- util/content.js
+++ util/content.js
@@ -11,7 +11,7 @@ const dir = require('./dir')
 
 // 封装读取文件内容方法
 
-const file  = require('./file')
+const file = require('./file')
 
 /**
  * 获取静态资源内容
@@ -20,30 +20,32 @@ const file  = require('./file')
  * @return {string} 请求获取到的本地内容
  */
 
- async function content(ctx,fullStaticPath) {
-     // 封装请求资源的完整的绝对路径
-     let reqPath = path.join(fullStaticPath,ctx.url)
-     // 判断请求路径是否为存在的目录或者文件
-     let exist = fs.existsSync(reqPath)
-     // 返回请求内容,默认为空
-     let content = ''
-     if (!exist){
+async function content(ctx, fullStaticPath) {
+    // 封装请求资源的完整的绝对路径
+    let reqPath = path.join(fullStaticPath, ctx.url)
+    // 判断请求路径是否为存在的目录或者文件
+    let exist = fs.existsSync(reqPath)
+    // 返回请求内容,默认为空
+    let content = ''
+    if (!exist) {
         // 如果请求的路径不存在,返回404
         content = '404 Not Found!'
-     }else {
-         // 判断访问地址是文件还是文件
-         // fs.statSync(path)  通过的stat(2)  返回一个fs.Stats实例
-         // 从 fs.stat()、fs.lstat()和 fs.fstat()及其同步版本返回的对象都是该类型
-         // stats.isFile()  stats.isDirectory()等
-         let stat = fs.statSync(reqPath)
-         if(stat.isCharacterDevice()){
-             // 如果为目录,则渲染读取目录内容
-             content = dir(ctx.url,reqPath)
+    } else {
+        // 判断访问地址是文件还是文件
+        // fs.statSync(path)  通过的stat(2)  返回一个fs.Stats实例
+        // 从 fs.stat()、fs.lstat()和 fs.fstat()及其同步版本返回的对象都是该类型
+        // stats.isFile()  stats.isDirectory()等
+        let stat = fs.statSync(reqPath)
+        if (stat.isCharacterDevice()) {
+            // 如果为目录,则渲染读取目录内容
+            content = dir(ctx.url, reqPath)
 
-         }else {
-             // 如果请求为文件,则读取文件内容
-             content  = await file(reqPath)
-         }
-     }
-     return content
- }
+        } else {
+            // 如果请求为文件,则读取文件内容
+            content = await file(reqPath)
+        }
+    }
+    return content
+}
+
+module.exports = content
--- util/dir.js
+++ util/dir.js
@@ -1,3 +1,32 @@
 /**
  * 读取目录内容
  */
+
+ const url = require('url')
+ const fs = require('fs')
+ const path = require('path')
+
+ // 遍历读取目录内容方法
+
+ const walk = require('./walk')
+
+ /**
+  * 封装目录内容
+  @params {string}URL当前请求的上下文中的URL,即 ctx.url
+  @ params {string} reqPath 请求静态资源的完整本地路径
+  @parms {string} 返回目录内容  封装程html
+  */
+
+  function  dir(url ,reqPath) {
+      // 遍历读取当前目录下的文件、子目录
+        let contentList = walk(reqPath)
+        let html = `<ul>`
+        for(let [index,item] of contentList.entries()){
+          html = `${html}<li><a href="${url === '/'?'':url}/${item}"></a>`  
+        }
+        html =`${html}</ul>`
+        
+        return html
+  }
+
+  module.exports = dir
