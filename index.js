4f5534034ac44daca05fa06bc2fbb509af3866d9
zhoujin <1391020381@qq.com>
2018-01-15 11:07:09 +0800

    koa-cookie

--- index.js
+++ index.js
@@ -8,13 +8,34 @@ app.use(koaBodyParser())
 
 const router = require('./router')
 
+app.use(async (ctx, next) => {
+  console.log('ctx.url:', ctx.url)
+  if (ctx.url === '/page/helloworld') {
+    ctx.cookies.set('cid', 'hello,world', {  //
+      domain: 'localhost',// 写cookie所在的域名
+      path: '/',     // 写cookie所在的路径
+      maxAge: 10 * 60 * 1000,  // cookie有效时长
+      expires: new Date('2018-2-15'),  // cookie失效时间
+      httpOnly: false,   // 是否只用于http请求中获取
+      overwrite: false  // 是否允许重写
+    })
+  }
+  next()
+})
 app.use(router.routes(), router.allowedMethods())
 
 // 静态资源目录对于相对入口文件index.js的路径
 const staticPath = './static'
-app.use(koaStatic(path.join( __dirname,  staticPath)))
+app.use(koaStatic(path.join(__dirname, staticPath)))
 // app.use(static())  //中间件必须是函数(static必须返回一个函数)
 
 app.listen(3000, () => {
   console.log(`koa2 is listening 3000`)
 })
+
+// koa2使用cookie
+
+
+
+
+
