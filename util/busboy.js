c07a98c4dfc13006b3ba2011affabb9e0b3b3e1a
zhoujin <1391020381@qq.com>
2018-01-18 10:47:01 +0800

    busboy-update

--- util/busboy.js
+++ util/busboy.js
@@ -17,8 +17,29 @@ busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
   console.log(`File [${fieldname}]:filename:${filename}`)
   //文件保存特定路径
   file.pipe(fs.createWriteStream('./upload'))
+
+  // 开始解析文件流
+  file.on('data', function (data) {
+    console.log(`File [${filedname}got${data.length} bytes]`)
+  })
+  // 解析文件结束
+  file.on('end', function () {
+    console.log(`File [${filedname}] Finished`)
+  })
 })
 
+// 监听请求中的字段
+busboy.on('filed', (fieldname, val, fieldnameTruncated, varTruncated) => {
+  console.log(`Field [${fieldname}]:value:${inspect(val)}`)
+})
+
+// 监听结束事件
+busboy.on('finish', function () {
+  console.log('Done parsing form!')
+  res.writeHead(303, {Connection: 'close', Location: '/'})
+  res.end()
+})
+req.pipe(busboy)
 
 
 
--- util/stream.md
+++ util/stream.md
@@ -1,5 +1,6 @@
 # 学习stream
 [nodeschool](https://nodeschool.io/)
+[csdn](http://blog.csdn.net/vieri_32/article/details/48376547)
 ## 简单stream进行pipe
 首先,我们可以通过管道将输入定位到输出,输入输出可以是控制台或者文件流或者http请求,比如:
 1. `process.stdin.pipe(process.stdout)`
@@ -10,4 +11,25 @@
 ```$xslt
 var through2 = require('through2')
 var stream = through2(write,end)
+process.stdin
+    .pipe(stream)
+    .pipe(process.stdout)
+    
+ function write(line,_,next){
+  this.push(line.toString().toUpperCase())
+  next()
+ } 
+ function end(done){
+ done()
+ }
+```
+## http server中的流
+类似stdin和fs,http由于其特性也适合使用流,所有其自带类似特性
+```$xslt
+var http = require('http')
+var server = http.createServer(function(req,res){
+req.pipe(res)
+
+})
+
 ```
