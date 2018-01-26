a18199ae3785ad331dd721da06268ccc950c35b6
zhoujin <1391020381@qq.com>
2018-01-26 15:12:28 +0800

    koa2-blog的基本结构

---
+++ koa2-blog/REAMD.md
@@ -0,0 +1,14 @@
+# koa2-blog
+1 koa node框架
+2. koa-bodyparser 表单解析中间件
+3. koa-mysql-session、koa-session-minimal 处理数据库的中间件
+4. koa-router 路由中间件
+5. koa-static 静态资源加载中间件
+6.ejs 模板引擎
+7. md5 密码加密
+8. moment 时间中间件
+9. mysql 数据库
+10. markdown-it markdown语法
+11. koa-views 模板呈现中间件
+12. chai mocha 测试使用
+13. koa-static-cache 文件缓存
--- koa2-blog/index.js
+++ koa2-blog/index.js
@@ -1,2 +1,47 @@
-/**
- * **/
+const Koa = require('koa')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
+const ejs = require('ejs')
+const session = require('koa-session-minimal')
+const MysqlStore = require('koa-mysql-session')
+const views = require('koa-views')
+const koaStatic = require('koa-static')
+const staticCache = require('koa-static-cache')
+const config = require('./config/default')
+
+const app = new Koa()
+
+// session存储配置
+const sessionMysqlConfig = {
+  user: config.database.USERNAME,
+  password: config.database.PASSWORD,
+  database: config.database.DATABASE,
+  host: config.database.HOST
+}
+
+// 配置session中间件
+
+app.use(session({
+  key: 'USER_SID',
+  store: new MysqlStore(sessionMysqlConfig)
+}))
+
+// 配置静态资源加载中间件
+app.use(koaStatic(path.join(__dirname, './public')))
+
+// 缓存
+
+app.use(staticCache(path.join(__dirname, './public'), {dynamic: true}, {maxAge: 365 * 24 * 60 * 60}))
+
+// 配置服务端模板渲染引擎的中间件
+
+app.use(views(path.join(__dirname, './views'), {
+  extensions: 'ejs'
+}))
+
+app.use(bodyParser({
+  formLimit:'1mb'
+}))
+
+// 路由
+app.use(require(path.join(__dirname,'./routes/signup.js')).routes())
