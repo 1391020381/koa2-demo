cd72d29d70e76dc2dd786e55b67ed63970626c7a
zhoujin <1391020381@qq.com>
2018-01-30 16:43:28 +0800

    signup-update

--- koa2-blog/routers/signup.js
+++ koa2-blog/routers/signup.js
@@ -5,6 +5,7 @@ const checkNotLogin = require('../middlewares/check').checkNotLogin
 const checkLogin = require('../middlewares/check').checkLogin
 const moment = require('moment')
 const fs = require('fs')
+const path = require('path')
 const router = new Router()
 //注册页面
 
@@ -20,6 +21,31 @@ router.post('/signup', async (ctx, next) => {
     repeatpass: ctx.request.body.repeatpass,
     avator: ctx.request.body.avator
   }
-  await userModel.findDataByName(user.name).then()
+  await userModel.findDataByName(user.name).then(async (result) => {
+    if (result.length) {
+      try {
+        throw Error('用户已存在')
+      } catch (error) {
+        console.log(error)
+      }
+      // 用户存在
+      ctx.body = {
+        data: 1
+      }
+    } else if (user.pass !== user.repeatpass || user.pass === '') {
+      ctx.body = {
+        data: 2
+      }
+    } else {
+      let base64Data = user.avator.replace(/^data:image\/\w+;base64,/);
+      let dataBuffer = new Buffer(base64Data, 'base64');
+      let getName = Number(Math.random().toString().substr(3).toString())
+      await fs.writeFile(path.join(__dirname, '/public/images/', getName, '.png'), dataBuffer, err => {
+        if (err) throw err
+        console.log('头像上传成功')
+      })
+      await userModel.insertData([user.name,md5(user.pass),getName,moment().format('YYYY-MM')])
+    }
+  })
 })
 module.exports = router
