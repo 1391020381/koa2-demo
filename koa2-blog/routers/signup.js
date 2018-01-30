af4efbde6602596d9c20665da747930c5d2d7c06
zhoujin <1391020381@qq.com>
2018-01-30 11:42:28 +0800

    signup-update

--- koa2-blog/routers/signup.js
+++ koa2-blog/routers/signup.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router')
-const router = new Router()
+const userModel = require('../lib/mysql')
+const md5 = require('md5')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 const checkLogin = require('../middlewares/check').checkLogin
 const moment = require('moment')
 const fs = require('fs')
-
+const router = new Router()
 //注册页面
 
 router.get('/signup', async (ctx, next) => {
@@ -12,4 +13,13 @@ router.get('/signup', async (ctx, next) => {
   await ctx.render('signup')
 })
 
+router.post('/signup', async (ctx, next) => {
+  let user = {
+    name: ctx.request.body.name,
+    pass: ctx.request.body.password,
+    repeatpass: ctx.request.body.repeatpass,
+    avator: ctx.request.body.avator
+  }
+  await userModel.findDataByName(user.name).then()
+})
 module.exports = router
